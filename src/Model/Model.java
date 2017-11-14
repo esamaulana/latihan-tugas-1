160f86f8b754891c28c0ab8269a48c46803b8c6e
Esa <Esa@Esa-PC>
2017-11-14 20:33:23 +0700

    Nilai Mahasiswa

---
+++ src/Model/Model.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Esa IF11 PBO 11
+ */
+public class Model {
+
+    String nim, nama;
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void modelMahasiswa() {
+        this.nim = nim;
+        this.nama = nama;
+    }
+
+}
